12d566db1124ec5b34398b731ef63ba99286acfe
Sterling <sterling@profitpulse.ai>
2026-03-16 15:06:39 -0500

    fix: add worker ESM dependencies (const.js, errors.js) for ffmpeg worker v0.7.4

--- utils/constants.js
+++ utils/constants.js
@@ -233,4 +233,4 @@ export const BADGE_RECORDING_COLOR = '#EF4444';
 export const EXTENSION_NAME = 'ScreenBolt';
 
 /** @type {string} Current version */
-export const VERSION = '0.7.3';
+export const VERSION = '0.7.4';
